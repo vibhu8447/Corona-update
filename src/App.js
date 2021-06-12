edea110e05e22af755c199f68cd6c99279f32ee8
vibhu sharma <vibhusharma8447@gmail.com>
2021-06-12 22:24:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { async } from 'q';
+import { map } from 'async';
+import BaseComponent from 'bootstrap/js/dist/base-component';
+import { Component } from 'react';
+
+const App=()=> {
+      const [data,setdata]=useState([]);
+      const getcoviddata=async ()=>
+      {
+        const res= await fetch('https://api.covid19india.org/data.json');
+        const actualdata=await res.json();
+        console.log(actualdata.statewise);
+        setdata(actualdata.statewise);
+        
+      }
+      useEffect(()  =>
+        {
+            getcoviddata();
+        },[]);
+
+  return(
+  <>
+  <div className='heading'>
+    <h1 className='title' ><span>INDIA</span> CORONA UPDATE </h1>
+  </div>
+  <div className='table-responsive' >
+      <table className='center'>
+        <thead className='table-dark'>
+        <tr>
+          <th>State</th>
+          <td>Confirmed</td>
+          <td>Recovered</td>
+          <td>Deaths</td>
+          <td>Active</td>
+          <td>Updates</td>
+        </tr>
+
+        </thead>
+        <tbody>
+
+          {
+
+            data.map((curE,index)=>
+            {
+                return (
+                    <tr>
+                      <th>{curE.state}</th>
+                      <td>{curE.confirmed}</td>
+                      <td>{curE.recovered}</td>
+                      <td>{curE.deaths>1000}</td>
+                      <td>{curE.active}</td>
+                      <td>{curE.lastupdatedtime}</td>
+
+
+                    </tr>
+                )
+            })
+          }
+        </tbody>
+
+      </table>
+  </div>
+  
+  </>
+  );
+}
+
+export default App;
